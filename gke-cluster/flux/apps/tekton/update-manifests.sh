364c545650f0c1cc1ab00fe65fd201482146606a
Patrick Fenerty <patrick@fenerty.me>
2026-04-04 15:23:43 -0500

    add tekton pruner

---
+++ gke-cluster/flux/apps/tekton/update-manifests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Update Tekton manifests
+curl -Lo pipelines.yaml https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+curl -Lo triggers.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+curl -Lo dashboard.yaml https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
+curl -Lo interceptors.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+curl -Lo pruner.yaml https://infra.tekton.dev/tekton-releases/pruner/latest/release.yaml
